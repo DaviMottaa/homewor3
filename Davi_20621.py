bad94af4af160af8da33033fbca1dc0c22b3611a
DaviMottaa <84351196+DaviMottaa@users.noreply.github.com>
2021-06-07 15:19:01 -0300

    Add files via upload

---
+++ Davi_20621.py
@@ -0,0 +1,37 @@
+# Question 2
+Carros = ["Toyota", "Honda","Ford","Nissan", "Chevrolet", "Bmw"]
+
+# Question 3
+print(Carros[1])
+
+# Question 4
+Carros.append("Bycycle")
+
+# Question 5
+print(Carros)
+
+# Question 6
+Carros.insert(0, "Davi")
+
+# Question 7
+print(Carros)
+
+# Question 8
+Carros2 = Carros[0]+" "+Carros[1]
+
+# Question 9 
+print(Carros2)
+
+# Question 10
+name=Carros[-1]
+
+# Question 11
+print(name)
+
+# Question 12
+Carros.insert(2, "Nova Odessa")
+
+#Question 13
+print(Carros)
+
+
